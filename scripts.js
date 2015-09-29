47439bfbafa721d02738e878cd21962a1365ef44
oravaantti <orava.antti@gmail.com>
2015-09-29 15:02:21 +0300

    added jquery and json-esim

---
+++ scripts.js
@@ -0,0 +1,26 @@
+/*Ready function is called when DOM is ready*/
+$(document).ready(function() {
+    console.log("We are ready");
+});
+
+/*
+
+$(document).ready(ready_yeah);
+
+function ready_yeah() {
+
+}
+
+*/
+
+/*
+Browser api for DOM ready event
+window.onload = function(event) {
+    console.log(event);
+    
+    var temp = document.getElementById("my_title");
+    temp.innerHTML = "Changed from JS";
+    
+    my_title.innerHTML = "Just the same";
+}
+*/
