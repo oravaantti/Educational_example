cac4150ffde087257b122304f385d29776fdff04
oravaantti <orava.antti@gmail.com>
2015-10-07 14:41:50 +0300

    jquery things done

---
+++ JSON-esim.js
@@ -0,0 +1,25 @@
+{
+    name: String,
+    address: String,
+    age: Number,
+    date: Date,
+    wokrplace: [{
+        work_address: String,
+        foreman: String,
+        start_date: Data
+    }],
+    callback: hello
+}
+
+function hello() {
+
+}
+
+data.name;
+date.workplace[0].work_address;
+
+for(var i = 0 ; i <= data.workplace.length ; i++) {
+
+}
+
+data.callback();
--- scripts.js
+++ scripts.js
@@ -1,6 +1,64 @@
 /*Ready function is called when DOM is ready*/
-$(document).ready(function() {
+$(function() {
     console.log("We are ready");
+    /*
+     1. We need to make an ajax (http) request to our mongo database
+     2. We need to generate UI dynamically from response data
+     To use HTTP api start mongod with: mongod.exe --rest --jsonp
+    */
+    
+    $.ajax({
+      url: "http://localhost:28017/person/friends/",
+      method: "GET",
+      dataType: "jsonp",
+      jsonp: "jsonp",
+      beforeSend: function() { console.log("START"); },
+      //success: function(data,text) { console.log(data); console.log(text); },
+      //error: function(XHR,text,error) { console.log(XHR); console.log(text); console.log(error); }
+    }).done(function(data) {
+        console.log("DONE");
+        
+        var length = data.rows.length;
+        /*
+        $(data.rows).each(function(i, elem) {
+            console.log(i);   
+            console.log(elem);
+            
+            $("#dataTable").appendTo();
+        });
+        */
+        /*
+        for(var i = 0 ; i < length ; i++) {
+            $("#dataTable").append(
+                "<tr><td>" + data.rows[i].name + "</td>" +
+                "<td>" + data.rows[i].address + "</td>" +
+                "<td>" + data.rows[i].age + "</td></tr>"
+            );
+        }
+        */
+        
+        
+        
+        $.each(data.rows, function(i, item) {
+            var classToAdd;
+            
+            if(item.age < 21) classToAdd = "young";
+            else if(item.age > 50) classToAdd = "old";
+            else classToAdd = "middle-age";
+            
+            var color = "#" + Math.round(item.age * 256 / 100).toString(16) + (256 - Math.round(item.age * 256 / 100)).toString(16) + "00";
+            
+            console.log(color);
+            
+            //var $tr = $("<tr class='" + classToAdd + "'>").append(
+            var $tr = $("<tr style='background-color:" + color + "'>").append(
+            $("<td>").text(item.name),
+            $("<td>").text(item.address),
+            $("<td>").text(item.age)
+            ).appendTo("#dataTable");
+        });
+        
+    });
 });
 
 /*
